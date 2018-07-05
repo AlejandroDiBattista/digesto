ae3bb7d335f661284fccbffa9baf2af6aa7fe22b
Alejandro Di Battista <alejandrodibattista@gmail.com>
2018-07-05 09:29:06 -0300

    Extraer documentos por categorias

---
+++ discapacidad/Tareas.rb
@@ -0,0 +1,9 @@
+Discapacidad:
+- Alejandro:
+  - Carperta Compartida a  mv.eventosyservicios@gmail.com
+  - Listado de ordenanzas
+  - Contactar Sofia Simon
+  - Contactar Burro / +54 9 381 413 5312‬
+- Valeria: 
+  - Revisar Ordenanzar
+  - Interiosarse "Transforamdores Sociales"
